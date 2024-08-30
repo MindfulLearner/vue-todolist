ead9717ffbeaddd233bb736b8bf48089345a44ff
MindfulLearner <dimajosh1009@gmail.com>
2024-08-30 19:41:26 +0200

    swag love of coding

---
+++ data.js
@@ -0,0 +1,23 @@
+// data
+
+const data = [
+    { testo: 'ciao',
+        done: true
+    },
+    { testo: 'supercan',
+        done: true
+    },
+    { testo: 'something',
+        done:true
+    },
+    { testo: 'swadawd',
+        done: true
+    },
+    { testo: 'swageger',
+        done: true
+    }
+];
+
+
+
+
---
+++ script.js
@@ -0,0 +1,32 @@
+// script
+
+const { createApp } = Vue; 
+
+createApp (
+    {
+        data() {
+            return {
+                todo: data,
+            }
+        },
+        methods: {
+            fatto(i) {
+                this.todo[i].done = false
+            }
+        }
+    }
+).mount('#apper');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
