c1acc965e9d9adac0a0563bfa6a0eec3221bc696
MindfulLearner <dimajosh1009@gmail.com>
2024-08-30 20:26:57 +0200

    init code ending

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 // script
 
-const { createApp } = Vue; 
+const { createApp } = Vue;
 
-createApp (
+createApp(
     {
         data() {
             return {
                 todo: data,
+                    testo: '',
+                    done: true
+                  
             }
         },
         methods: {
             fatto(i) {
-                this.todo[i].done = false
+                this.todo[i].done = false;
+            },
+            rimuovi(i) {
+                this.todo.splice(i, 1);
+            },
+            aggiungi(){
+                if (this.testo.trim()) {
+                this.todo.push({ testo: this.testo, done: this.done});
+                console.log(this.todo);
+                this.testo = '';
+                }
             }
         }
     }
