d233ff73c65e53e52be57ec316168824967b0b5b
MindfulLearner <dimajosh1009@gmail.com>
2024-08-30 20:35:41 +0200

    aggiunti commenti love of coding

--- script.js
+++ script.js
@@ -6,40 +6,35 @@ createApp(
     {
         data() {
             return {
+                // inizializzo todo con data separata in una altro js 
                 todo: data,
-                    testo: '',
-                    done: true
-                  
+                // testo vuoto e done true
+                testo: '',
+                done: true
             }
         },
         methods: {
+            // funzione che mi fa mettere le linea sulla scritta 
             fatto(i) {
                 this.todo[i].done = false;
             },
+            // funzione che rimuove quello selezionato
             rimuovi(i) {
                 this.todo.splice(i, 1);
             },
+            // funzione che mi fa aggoingere attravero input, usiamo codnizione se trim cosi non viene aggiunto stringa vuota 
             aggiungi(){
                 if (this.testo.trim()) {
-                this.todo.push({ testo: this.testo, done: this.done});
-                console.log(this.todo);
-                this.testo = '';
+                    // pushing new text
+                    this.todo.push({testo: this.testo, done: this.done});
+                    console.log(this.todo);
+                    //rimuoviamo spazi per scrivere ancora
+                    this.testo = '';
                 }
             }
         }
     }
 ).mount('#apper');
-
-
-
-
-
-
-
-
-
-
-
 
 
 
